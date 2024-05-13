e7b78df04bd3e217d07a7f23f9fafe5baadf2abe
Rahul Mohan <rahul.mohan@se.abb.com>
2024-05-13 14:28:51 +0200

    Corrected the connecting edge dimensions

--- nodeeditor/node_graphics_edge_path.py
+++ nodeeditor/node_graphics_edge_path.py
@@ -45,34 +45,30 @@ class GraphicsEdgePathBezier(GraphicsEdgePathBase):
         """
         s = self.owner.posSource
         d = self.owner.posDestination
-        dist = (d[0] - s[0]) * 0.5
+        dist = (d[1] - s[1]) * 0.5  # Calculate half of the vertical distance
 
-        cpx_s = +dist
-        cpx_d = -dist
-        cpy_s = 0
-        cpy_d = 0
+        cpx_s = 0
+        cpx_d = 0
+        cpy_s = +dist  # Adjust control point for source position
+        cpy_d = -dist  # Adjust control point for destination position
 
         if self.owner.edge.start_socket is not None:
             ssin = self.owner.edge.start_socket.is_input
             ssout = self.owner.edge.start_socket.is_output
 
-            if (s[0] > d[0] and ssout) or (s[0] < d[0] and ssin):
-                cpx_d *= -1
-                cpx_s *= -1
-
-                cpy_d = (
-                    (s[1] - d[1]) / math.fabs(
-                        (s[1] - d[1]) if (s[1] - d[1]) != 0 else 0.00001
+            if (s[1] > d[1] and ssout) or (s[1] < d[1] and ssin):  # Check direction
+                cpx_d = (
+                    (s[0] - d[0]) / math.fabs(
+                        (s[0] - d[0]) if (s[0] - d[0]) != 0 else 0.00001
                     )
-                ) * EDGE_CP_ROUNDNESS
-                cpy_s = (
-                    (d[1] - s[1]) / math.fabs(
-                        (d[1] - s[1]) if (d[1] - s[1]) != 0 else 0.00001
+                ) * EDGE_CP_ROUNDNESS  # Adjust control point for destination position
+                cpx_s = (
+                    (d[0] - s[0]) / math.fabs(
+                        (d[0] - s[0]) if (d[0] - s[0]) != 0 else 0.00001
                     )
-                ) * EDGE_CP_ROUNDNESS
-
+                ) * EDGE_CP_ROUNDNESS  # Adjust control point for source position
 
         path = QPainterPath(QPointF(self.owner.posSource[0], self.owner.posSource[1]))
-        path.cubicTo( s[0] + cpx_s, s[1] + cpy_s, d[0] + cpx_d, d[1] + cpy_d, self.owner.posDestination[0], self.owner.posDestination[1])
+        path.cubicTo(s[0] + cpx_s, s[1] + cpy_s, d[0] + cpx_d, d[1] + cpy_d, self.owner.posDestination[0], self.owner.posDestination[1])
 
         return path
